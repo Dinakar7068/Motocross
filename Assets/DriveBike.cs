5cbb03bfa931acf91d4ba42f292b3cbd306b54ac
Dinakar7068 <dinakargubendran7068@gmail.com>
2025-05-24 21:32:11 +0530

    first

---
+++ Assets/DriveBike.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using UnityEngine;
+
+public class DriveBike : MonoBehaviour
+{
+    [SerializeField] private Rigidbody2D frontTireRB;
+    [SerializeField] private Rigidbody2D backTireRB;
+    [SerializeField] private Rigidbody2D carRB;
+    [SerializeField] private float speed = 150f;
+    [SerializeField] private float rotationSpeed = 300f;
+
+    private float moveInput;
+
+    void Update()
+    {
+        moveInput = Input.GetAxisRaw("Horizontal");
+    }
+
+    private void FixedUpdate()
+    {
+        frontTireRB.AddTorque(-moveInput * speed * Time.fixedDeltaTime);
+        backTireRB.AddTorque(-moveInput * speed * Time.fixedDeltaTime);
+        carRB.AddTorque(moveInput * rotationSpeed * Time.fixedDeltaTime);
+    }
+}
